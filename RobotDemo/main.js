7117ff911eefc93329095c577f01979cb4ce5672
dungnguyen234 <dungnguyen@strongtie.com>
2022-03-09 21:17:25 +0700

    change robot marker

--- RobotDemo/main.js
+++ RobotDemo/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './asset/targets/targets (1).mind',
+      imageTargetSrc: './asset/targets/robot.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
