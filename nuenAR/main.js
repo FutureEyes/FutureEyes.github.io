fd24e0c2a5ce9a558369f1c6b4cda6d40d782e2b
ADMIN <dung.ntk234@gmail.com>
2022-05-10 14:58:06 +0700

    update missing content

--- nuenAR/main.js
+++ nuenAR/main.js
@@ -1,26 +1,21 @@
-import {loadGLTF} from "./libs/loader.js";
-import {mockWithVideo} from './libs/camera-mock';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
-    //mockWithVideo('../../assets/mock-videos/musicband1.mp4');
-
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-      container: document.body,
-      imageTargetSrc: './assets/targets/musicband.mind',
+      container: document.querySelector("#container"),
+      imageTargetSrc: './assets/targets/nuen.mind',
+      uiScanning: "#scanning",
+      uiLoading: "no",
     });
     const {renderer, scene, camera} = mindarThree;
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-    scene.add(light);
-
-    const raccoon = await loadGLTF('./assets/models/nuen/NUEN.glb');
-    raccoon.scene.scale.set(0.1, 0.1, 0.1);
-    raccoon.scene.position.set(0, -0.4, 0);
+    const geometry = new THREE.PlaneGeometry(1, 1);
+    const material = new THREE.MeshBasicMaterial({color: 0x00ffff, transparent: true, opacity: 0.5});
+    const plane = new THREE.Mesh(geometry, material);
 
     const anchor = mindarThree.addAnchor(0);
-    anchor.group.add(raccoon.scene);
+    anchor.group.add(plane);
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
